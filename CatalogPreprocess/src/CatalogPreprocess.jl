39e9995e2cb10e17dd5a7f885e92098503a8e69f
okatsn <okatsn@gmail.com>
2025-12-09 10:05:53 +0800

    Add `dir_data_catalogs`.

--- CatalogPreprocess/src/CatalogPreprocess.jl
+++ CatalogPreprocess/src/CatalogPreprocess.jl
@@ -2,7 +2,7 @@ module CatalogPreprocess
 
 using Pkg
 include("projdir.jl")
-export dir_data, dir_data_intermediate, dir_data_raw, dir_proj
+export dir_data, dir_data_intermediate, dir_data_raw, dir_data_catalogs, dir_proj
 
 # Write your package code here.
 
