9ce3904a52fd7e348e20bf09c56c08ceecebe3c3
okatsn <okatsn@gmail.com>
2025-12-09 19:07:20 +0800

    Transform `epochday` and sort the table by `ML`

--- CatalogPreprocess/scripts/raw_catalog_preview.jl
+++ CatalogPreprocess/scripts/raw_catalog_preview.jl
@@ -12,6 +12,8 @@ df0 = @chain raws begin
     CSV.read.(_, DataFrame)
     reduce(vcat, _)
     transform!(:date => ByRow(x -> (year=year(x), month=month(x))) => AsTable)
+    transform!(:date => ByRow(Dates.date2epochdays) => :epochday)
+    sort!(:ML, rev=true) # ensure small event on top
 end
 
 function main()
