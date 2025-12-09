9a8a38922085294d96c99e7507b78f29c3ed36a7
okatsn <okatsn@gmail.com>
2025-12-09 12:26:08 +0800

    Transform the year and month columns.

--- CatalogPreprocess/scripts/raw_catalog_preview.jl
+++ CatalogPreprocess/scripts/raw_catalog_preview.jl
@@ -4,10 +4,12 @@ using CairoMakie
 using AlgebraOfGraphics
 using CatalogPreprocess
 using Chain
+using Dates
 
 raws = filelist(r"catalog.*\.csv$", dir_data_raw())
 
 df0 = @chain raws begin
     CSV.read.(_, DataFrame)
     reduce(vcat, _)
+    transform!(:date => ByRow(x -> (year=year(x), month=month(x))) => AsTable)
 end
